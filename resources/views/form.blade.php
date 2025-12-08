d23f929a3df14f26e8f0587000b2b87d5899e5b5
hamanhcoder321 <vanmanh8930@gmail.com>
2025-12-08 16:52:09 +0700

    code moi

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -12,28 +12,28 @@
         <form method="POST" action="{{ route('form.submit') }}">
             @csrf
 
-            <label>Họ và tên:</label><br>
-            <input type="text" name="name" value="{{ old('name', session('name')) }}">
+            <label>Họ và tên (*):</label><br>
+            <input type="text"  name="name"  value="{{ old('name', session('name')) }}">
             @error('name')
                 <div style="color:red;">{{ $message }}</div>
             @enderror
             <br><br>
 
-            <label>Email:</label><br>
-            <input type="text" name="email" value="{{ old('email', session('email')) }}">
+            <label>Email (*):</label><br>
+            <input type="text" name="email"  value="{{ old('email', session('email')) }}">
             @error('email')
             <div style="color: red;">{{ $message }}</div>
             @enderror
             <br><br>
 
-            <label>Tuổi:</label><br>
-            <input type="text" name="age" value="{{ old('age', session('age')) }}">
+            <label>Tuổi (*):</label><br>
+            <input type="text" name="age"  value="{{ old('age', session('age')) }}">
             @error('age')
             <div style="color: red;">{{ $message }}</div>
             @enderror
             <br><br>
 
-            <button type="submit">Submit</button>
+            <button class="submit-1" type="submit">Submit</button>
         </form>
     </div>
 
