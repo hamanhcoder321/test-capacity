04d90fd878c66e9b9b1e7a9244391b1268558cd7
hamanhcoder321 <vanmanh8930@gmail.com>
2025-12-08 16:13:19 +0700

    code mơi

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,26 +2,40 @@
 <html>
 <head>
     <title>Form</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
-<h2>Nhập thông tin</h2>
 
+    <div class="form-container">
+        <h2>Nhập thông tin</h2>
+        <form method="POST" action="{{ route('form.submit') }}">
+            @csrf
 
-<form method="POST" action="{{ route('form.submit') }}">
-    @csrf
+            <label>Họ và tên:</label><br>
+            <input type="text" name="name" value="{{ old('name', session('name')) }}">
+            @error('name')
+                <div style="color:red;">{{ $message }}</div>
+            @enderror
+            <br><br>
 
-    <label>Họ và tên:</label><br>
-    <input type="text" name="name" value="{{ old('name', session('name')) }}"><br><br>
+            <label>Email:</label><br>
+            <input type="text" name="email" value="{{ old('email', session('email')) }}">
+            @error('email')
+            <div style="color: red;">{{ $message }}</div>
+            @enderror
+            <br><br>
 
-    <label>Email:</label><br>
-    <input type="text" name="email" value="{{ old('email', session('email')) }}"><br><br>
+            <label>Tuổi:</label><br>
+            <input type="text" name="age" value="{{ old('age', session('age')) }}">
+            @error('age')
+            <div style="color: red;">{{ $message }}</div>
+            @enderror
+            <br><br>
 
-    <label>Tuổi:</label><br>
-    <input type="text" name="age" value="{{ old('age', session('age')) }}"><br><br>
-
-    <button type="submit">Submit</button>
-</form>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
 
 </body>
 </html>
